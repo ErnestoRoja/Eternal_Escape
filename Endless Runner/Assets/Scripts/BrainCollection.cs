7ef91dfb47a24b4e45943a40884790c603d68573
Ernesto Roja <ernestoroja101@gmail.com>
2024-02-20 20:08:47 -0500

    particle effects

--- Endless Runner/Assets/Scripts/BrainCollection.cs
+++ Endless Runner/Assets/Scripts/BrainCollection.cs
@@ -9,14 +9,17 @@ public class BrainCollection : MonoBehaviour
     public TextMeshProUGUI brainText;
     public SpawnManager spawnManager;
 
+    public ParticleSystem brainCollectParticle;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Brain")
         {
             Brain++;
             brainText.text = Brain.ToString();
-            Debug.Log(Brain);
+            Instantiate(brainCollectParticle, other.transform.position, other.transform.rotation);
             Destroy(other.gameObject);
+            
         }
     }
 }
